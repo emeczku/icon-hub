60f4cc8b5e1fcb5e91bfbc1b7c066c4fabb9ac5e
emeczku <dzius89@gmail.com>
2023-06-01 17:57:12 +0200

    create technologies array

---
+++ src/data/technologies.ts
@@ -0,0 +1,84 @@
+import type { Technology } from '@/types'
+
+export const technologies: Technology[] = [
+  {
+    name: 'GitHub',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg',
+    description:
+      'GitHub is an online platform and service that hosts repositories of source code for version control and collaboration.',
+    category: 'Development',
+  },
+  {
+    name: 'GitLab',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gitlab/gitlab-original.svg',
+    description:
+      'GitLab is a web-based DevOps platform that provides a complete set of tools for version control, continuous integration/continuous deployment (CI/CD), issue tracking, and more.',
+    category: 'Development',
+  },
+  {
+    name: 'Git',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
+    description:
+      'Git is a distributed version control system that is widely used for tracking changes in source code during software development. It was created by Linus Torvalds, the creator of the Linux operating system, and is designed to be fast, flexible, and efficient.',
+    category: 'Development',
+  },
+  {
+    name: 'JavaScript',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+    description:
+      'JavaScript is a high-level, interpreted programming language that is primarily used for creating interactive and dynamic elements on websites. It is often referred to as the language of the web because it is supported by all major web browsers and enables the creation of dynamic web pages.',
+    category: 'Front-end',
+  },
+  {
+    name: 'TypeScript',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
+    description:
+      'TypeScript is a programming language that is a typed superset of JavaScript. It adds optional static typing to JavaScript, providing additional features and tools to help developers write more robust and scalable code. TypeScript is developed and maintained by Microsoft.',
+    category: 'Full-stack',
+  },
+  {
+    name: 'HTML',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
+    description:
+      'HTML, short for HyperText Markup Language, is the standard markup language used to create the structure and content of web pages. It defines the elements and layout of a web page by using a set of tags and attributes.',
+    category: 'Front-end',
+  },
+  {
+    name: 'CSS',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg',
+    description:
+      'CSS, short for Cascading Style Sheets, is a style sheet language used to describe the presentation and visual appearance of a document written in HTML or XML. It allows web developers to control the layout, colors, fonts, and other visual aspects of web pages.',
+    category: 'Front-end',
+  },
+  {
+    name: 'Sass',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg',
+    description:
+      'Sass, which stands for "Syntactically Awesome Style Sheets," is a preprocessor scripting language that extends the capabilities of CSS. It provides additional features and syntax that make it easier and more efficient to write CSS stylesheets.',
+    category: 'Front-end',
+  },
+  {
+    name: 'Tailwind CSS',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg',
+    description:
+      'Tailwind CSS is a utility-first CSS framework that provides a set of pre-built, atomic-level utility classes. It aims to provide a highly customizable and efficient way to build modern user interfaces by focusing on utility classes rather than predefined components.',
+    category: 'Front-end',
+  },
+  {
+    name: 'Bootstrap',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg',
+    description:
+      'Bootstrap is a popular open-source front-end framework that provides a collection of pre-built components and tools for building responsive and mobile-first websites and web applications. It was originally developed by Twitter and is now maintained by a community of developers.',
+    category: 'Front-end',
+  },
+]
---
+++ src/types/index.d.ts
@@ -0,0 +1,6 @@
+export interface Technology {
+  name: string
+  image: string
+  description: string
+  category: string
+}
