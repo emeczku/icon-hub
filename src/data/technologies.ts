2fd81072839bf8bbdf9c3404926ad90a4a759802
emeczku <dzius89@gmail.com>
2023-07-02 19:38:35 +0200

    add technologies

--- src/data/technologies.ts
+++ src/data/technologies.ts
@@ -35,7 +35,7 @@ export const technologies: Technology[] = [
       'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
     description:
       'JavaScript is a high-level, interpreted programming language that is primarily used for creating interactive and dynamic elements on websites. It is often referred to as the language of the web because it is supported by all major web browsers and enables the creation of dynamic web pages.',
-    category: 'Front-end',
+    category: 'Web Development',
   },
   {
     id: 5,
@@ -44,7 +44,7 @@ export const technologies: Technology[] = [
       'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
     description:
       'TypeScript is a programming language that is a typed superset of JavaScript. It adds optional static typing to JavaScript, providing additional features and tools to help developers write more robust and scalable code. TypeScript is developed and maintained by Microsoft.',
-    category: 'Full-stack',
+    category: 'Web Development',
   },
   {
     id: 6,
@@ -53,7 +53,7 @@ export const technologies: Technology[] = [
       'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
     description:
       'HTML, short for HyperText Markup Language, is the standard markup language used to create the structure and content of web pages. It defines the elements and layout of a web page by using a set of tags and attributes.',
-    category: 'Front-end',
+    category: 'Web Development',
   },
   {
     id: 7,
@@ -62,7 +62,7 @@ export const technologies: Technology[] = [
       'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg',
     description:
       'CSS, short for Cascading Style Sheets, is a style sheet language used to describe the presentation and visual appearance of a document written in HTML or XML. It allows web developers to control the layout, colors, fonts, and other visual aspects of web pages.',
-    category: 'Front-end',
+    category: 'Web Development',
   },
   {
     id: 8,
@@ -71,7 +71,7 @@ export const technologies: Technology[] = [
       'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg',
     description:
       'Sass, which stands for "Syntactically Awesome Style Sheets," is a preprocessor scripting language that extends the capabilities of CSS. It provides additional features and syntax that make it easier and more efficient to write CSS stylesheets.',
-    category: 'Front-end',
+    category: 'Web Development',
   },
   {
     id: 9,
@@ -80,7 +80,7 @@ export const technologies: Technology[] = [
       'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg',
     description:
       'Tailwind CSS is a utility-first CSS framework that provides a set of pre-built, atomic-level utility classes. It aims to provide a highly customizable and efficient way to build modern user interfaces by focusing on utility classes rather than predefined components.',
-    category: 'Front-end',
+    category: 'Web Development',
   },
   {
     id: 10,
@@ -89,6 +89,105 @@ export const technologies: Technology[] = [
       'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg',
     description:
       'Bootstrap is a popular open-source front-end framework that provides a collection of pre-built components and tools for building responsive and mobile-first websites and web applications. It was originally developed by Twitter and is now maintained by a community of developers.',
-    category: 'Front-end',
+    category: 'Web Development',
+  },
+  {
+    id: 11,
+    name: 'Azure',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original.svg',
+    description:
+      'Microsoft Azure is a cloud computing platform and service offered by Microsoft. It provides a wide range of cloud services, including virtual machines, storage, databases, AI, and more. Azure enables businesses to build, deploy, and manage applications and services through Microsoft-managed data centers.',
+    category: 'Cloud Computing',
+  },
+  {
+    id: 12,
+    name: 'Babel',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/babel/babel-original.svg',
+    description:
+      'Babel is a JavaScript compiler that enables developers to write next-generation JavaScript (ES6+) code and convert it into backward-compatible versions of JavaScript that can run in older browsers or environments. It allows developers to use modern JavaScript features while ensuring compatibility with a wide range of target environments.',
+    category: 'Development',
+  },
+  {
+    id: 13,
+    name: 'Behance',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/behance/behance-original.svg',
+    description:
+      'Behance is an online platform owned by Adobe that allows creative professionals to showcase and discover creative work. It is widely used by artists, designers, photographers, and other creative individuals to share their portfolios, connect with other professionals, and find inspiration.',
+    category: 'Design',
+  },
+  {
+    id: 14,
+    name: 'C',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg',
+    description:
+      'C is a general-purpose programming language that has been widely used for developing a wide range of applications, including operating systems, embedded systems, and high-performance software. It is known for its low-level control over computer hardware and efficient memory management.',
+    category: 'Development',
+  },
+  {
+    id: 15,
+    name: 'C#',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg',
+    description:
+      'C# (pronounced C sharp) is a modern, object-oriented programming language developed by Microsoft. It is widely used for building Windows applications, web services, and game development using the Unity game engine. C# is known for its simplicity, type safety, and strong integration with the .NET framework.',
+    category: 'Development',
+  },
+  {
+    id: 16,
+    name: 'C++',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg',
+    description:
+      'C++ is a general-purpose programming language that is an extension of the C programming language. It supports object-oriented programming, generic programming, and low-level system programming. C++ is widely used for developing high-performance software, games, embedded systems, and more.',
+    category: 'Development',
+  },
+  {
+    id: 17,
+    name: 'Docker',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg',
+    description:
+      'Docker is an open-source platform that automates the deployment, scaling, and management of applications using containerization. It allows developers to package applications and their dependencies into lightweight, isolated containers that can run consistently on any infrastructure. Docker simplifies the deployment process and promotes consistency and reproducibility across different environments.',
+    category: 'DevOps',
+  },
+  {
+    id: 18,
+    name: 'Django',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg',
+    description:
+      'Django is a high-level web framework written in Python. It follows the model-template-view architectural pattern and provides a robust set of tools and features for building web applications efficiently. It is widely used for developing complex, database-driven websites and web applications.',
+    category: 'Web Development',
+  },
+  {
+    id: 19,
+    name: 'Express',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg',
+    description:
+      'Express is a fast and minimalist web application framework for Node.js. It provides a simple and flexible way to build web applications and APIs. Express is known for its unopinionated nature, allowing developers to have full control over the application structure and behavior. It is widely used in combination with Node.js for building server-side applications.',
+    category: 'Web Development',
+  },
+  {
+    id: 20,
+    name: 'Figma',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg',
+    description:
+      'Figma is a cloud-based design and prototyping tool used for creating user interfaces, interactive prototypes, and collaborative design workflows. It provides a powerful set of features for designing and iterating on designs, including real-time collaboration, design versioning, and developer handoff. Figma is widely used by designers and design teams to create digital designs and collaborate effectively.',
+    category: 'Design',
+  },
+  {
+    id: 21,
+    name: 'Firebase',
+    image:
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg',
+    description:
+      'Firebase is a comprehensive suite of cloud-based services provided by Google for building web and mobile applications. It offers a wide range of features, including real-time database, cloud storage, authentication, hosting, and more. Firebase simplifies the backend development process by providing pre-built infrastructure and APIs, allowing developers to focus on building the frontend of their applications.',
+    category: 'Cloud Computing',
   },
 ]
