124f26b0cf0246183c9a447bf2f05f88ad59d0f0
emeczku <dzius89@gmail.com>
2023-07-03 09:59:22 +0200

    sort alphabetically

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,20 @@ import IconsContainer from '@/components/IconsContainer'
 export default function Home() {
   const technologies = useSelector(filteredTechnologies)
 
+  const sortedTechnologies = [...technologies].sort(function (a, b) {
+    if (a.name < b.name) {
+      return -1
+    }
+    if (a.name > b.name) {
+      return 1
+    }
+    return 0
+  })
+
   return (
     <>
       <SearchInput />
-      <IconsContainer technologies={technologies} />
+      <IconsContainer technologies={sortedTechnologies} />
     </>
   )
 }
